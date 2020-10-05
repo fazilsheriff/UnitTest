d0d6ba05cc097e4c7b4b59aea8510bbc7e0e88ee
fazil.s@lantrasoft.com <SinCos123>
2020-10-05 16:38:29 +0530

    Fitness

---
+++ app/src/main/java/com/example/junit/FitnessTracker.java
@@ -0,0 +1,17 @@
+package com.example.junit;
+
+public class FitnessTracker {
+    public static final int RUN_STEPS_FACTOR = 2;
+
+    public void step() {
+        Counter.getInstance().add();
+    }
+
+    public void runStep() {
+        Counter.getInstance().add(RUN_STEPS_FACTOR);
+    }
+
+    public int getTotalSteps() {
+        return Counter.getInstance().getTotal();
+    }
+}
---
+++ app/src/test/java/com/example/junit/FitnessTrackerTest.java
@@ -0,0 +1,29 @@
+package com.example.junit;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class FitnessTrackerTest {
+    FitnessTracker SUT;
+
+    @Before
+    public void setUp() throws Exception {
+
+        SUT=new FitnessTracker();
+    }
+
+    @Test
+    public void total_Incremented() {
+        SUT.step();
+        assertThat(SUT.getTotalSteps(),is(1));
+    }
+
+    @Test
+    public void runStepTotalIncremented() {
+        SUT.runStep();
+        assertThat(SUT.getTotalSteps(),is(2));
+    }
+}
