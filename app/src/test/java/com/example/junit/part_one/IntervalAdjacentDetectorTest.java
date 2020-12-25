41fad02ce6a2369e428005f667022da65a43a983
fazil.s@lantrasoft.com <SinCos123>
2020-12-25 09:53:30 +0530

    IntervalAdjacentDetector class

---
+++ app/src/test/java/com/example/junit/part_one/IntervalAdjacentDetectorTest.java
@@ -0,0 +1,42 @@
+package com.example.junit.part_one;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class IntervalAdjacentDetectorTest {
+IntervalAdjacentDetector SUT;
+    @Before
+    public void setUp() throws Exception {
+        SUT=new IntervalAdjacentDetector();
+    }
+    
+    //Check both interval star and end are same
+
+
+    @Test
+    public void isEqual_interval1StartAndEndAreEqual_falseReturned() {
+        Interval interval1=new Interval(18,150);
+        Interval interval2=new Interval(18,100);
+        boolean result=SUT.isAdjacent(interval1,interval2);
+        assertThat(result,is(false));
+    }
+
+    @Test
+    public void isEqual_intervalEndsAndEndAreEqual_falseReturned() {
+        Interval interval1=new Interval(38,100);
+        Interval interval2=new Interval(48,100);
+        boolean result=SUT.isAdjacent(interval1,interval2);
+        assertThat(result,is(false));
+    }
+
+    @Test
+    public void isEqual_checkingTwoIntervlasSame_falseReturned() {
+        Interval interval1=new Interval(38,100);
+        Interval interval2=new Interval(38,100);
+        boolean result=SUT.isAdjacent(interval1,interval2);
+        assertThat(result,is(false));
+    }
+}
--- app/src/test/java/com/example/junit/part_two/example4/LoginUseCaseSyncTest.java
+++ app/src/test/java/com/example/junit/part_two/example4/LoginUseCaseSyncTest.java
@@ -5,41 +5,54 @@ import com.example.junit.part_two.example4.eventbus.EventBusPoster;
 import com.example.junit.part_two.example4.networking.LoginHttpEndpointSync;
 import com.example.junit.part_two.example4.networking.NetworkErrorException;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class LoginUseCaseSyncTest {
-LoginUseCaseSync SUT;
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "Password";
+
+    LoginUseCaseSync SUT;
+    LoginHttpEndPointSyncTd loginHttpEndPointSyncTd;
     @Before
     public void setUp() throws Exception {
-        SUT=new LoginUseCaseSync(new LoginHttpEndPointSyncTd(),new AuthTokenCacheTd(),new EventBusPosterTd());
+        loginHttpEndPointSyncTd=new LoginHttpEndPointSyncTd();
+        SUT=new LoginUseCaseSync(loginHttpEndPointSyncTd,new AuthTokenCacheTd(),new EventBusPosterTd());
+    }
+
+    @Test
+    public void loginSysnc_userNameAndPasswordPassedEndPoint_trueReturned() {
+
+        SUT.loginSync(USERNAME,PASSWORD);
+        Assert.assertThat(loginHttpEndPointSyncTd.muserName, CoreMatchers.is(USERNAME));
+        Assert.assertThat(loginHttpEndPointSyncTd.mPassword, CoreMatchers.is(PASSWORD));
     }
 
     private static  class LoginHttpEndPointSyncTd implements LoginHttpEndpointSync {
+        public String muserName;
+        public String mPassword;
 
         @Override
         public EndpointResult loginSync(String username, String password) throws NetworkErrorException {
+            muserName=username;
+            mPassword=password;
             return null;
         }
     }
-
     private  static class  AuthTokenCacheTd implements AuthTokenCache{
-
         @Override
         public void cacheAuthToken(String authToken) {
-
         }
-
         @Override
         public String getAuthToken() {
             return null;
         }
     }
-
-    private static class EventBusPosterTd implements EventBusPoster
-    {
-
+    private static class EventBusPosterTd implements EventBusPoster{
         @Override
         public void postEvent(Object event) {
 
